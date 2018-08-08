9517b58b199a096be2d237c2c5fcae8faf18a263
helenosheaa <helenosheaa@gmail.com>
2018-08-08 08:38:44 +0100

    displays one bullet point list of instruments

--- src/views/instrument_info_view.js
+++ src/views/instrument_info_view.js
@@ -14,11 +14,23 @@ InstrumentInfoView.prototype.bindEvents = function () {
 InstrumentInfoView.prototype.render = function (instrument) {
   const instrumentHeader = document.createElement('h1');
   const instrumentInfo = document.createElement('p');
+  const listHeader = document.createElement('h2');
+  // const musicalInstrumentsUL = document.createElement('ul');
+  const musicalInstrumentsList = document.createElement('li');
   instrumentHeader.textContent = `${instrument.name}`;
   instrumentInfo.textContent = `${instrument.description}`;
+  console.dir(instrument);
+  listHeader.textContent = `Instruments Including:`
+  musicalInstrumentsList.textContent = `${instrument.instruments}`
+  // instruments.forEach(function(instrument) {})
   this.container.innerHTML = '';
   this.container.appendChild(instrumentHeader);
   this.container.appendChild(instrumentInfo);
+  this.container.appendChild(listHeader);
+  this.container.appendChild(musicalInstrumentsList);
+  // musicalInstrumentsUL.appendChild(musicalInstrumentsList);
+
+
 };
 
 module.exports = InstrumentInfoView;
